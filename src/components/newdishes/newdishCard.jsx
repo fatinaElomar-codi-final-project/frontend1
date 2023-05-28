e6b612e415a7c4c8501020046b4d9c901650d492
fatinaelomar3 <fatinaelomar3@gmail.com>
2023-05-28 16:44:20 +0300

    add populare dessert

--- src/components/newdishes/newdishCard.jsx
+++ src/components/newdishes/newdishCard.jsx
@@ -1,237 +1,51 @@
-import { NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-import "./cart.css";
+///new dshcard
 
-function Product() {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-  const [sortOption, setSortOption] = useState("price-asc");
-  const [selectedCategory, setSelectedCategory] = useState("");
-  // const [category, setCategory] = useState("");
-  const allproduct = [...data];
-  const { category } = useParams();
-  useEffect(() => {
-    axios({
-      method: "GET",
-      baseURL: "http://localhost:8000/dish",
-    })
-      .then((response) => {
-        setData(response.data.response);
-        console.log(response.data.response);
-        setLoading(false);
-      })
-      .catch((err) => console.log(err));
-  }, []);
 
-  const getFilteredProducts = () => {
-    let filteredProducts = [...data];
+import React from 'react';
+import './cart.css';
 
-    // filter the data by selected category name
-    if (selectedCategory !== "") {
-      filteredProducts = filteredProducts.filter(
-        (product) => product.category.name === selectedCategory
-      );
-    }
-
-    return filteredProducts;
-  };
-
-  const getSortedProducts = () => {
-    let sortedProducts = [...getFilteredProducts()];
-
-    switch (sortOption) {
-      case "price-asc":
-        sortedProducts.sort((a, b) => a.price - b.price);
-        break;
-      case "price-desc":
-        sortedProducts.sort((a, b) => b.price - a.price);
-        break;
-      case "title-asc":
-        sortedProducts.sort((a, b) => {
-          if (a.title && b.title) {
-            return a.title.localeCompare(b.title);
-          } else {
-            return 0;
-          }
-        });
-        break;
-      case "title-desc":
-        sortedProducts.sort((a, b) => {
-          if (b.title && a.title) {
-            return b.title.localeCompare(a.title);
-          } else {
-            return 0;
-          }
-        });
-        break;
-    }
-
-    return sortedProducts;
-  };
-
-  const handleSortOptionChange = (event) => {
-    setSortOption(event.target.value);
-  };
-
-  const handleCategoryChange = (event) => {
-    const selectedValue = event.target.value;
-    setSelectedCategory(selectedValue);
-  };
+const newdishCard = (props) => {
+  const { name, src, price } = props.item;
 
   return (
-    <div className="Shop-container">
-      <div className="products">
-        <h1>Products</h1>
-        <div className="categories-sort">
-          <div className="Categories-links">
-            <nav>
-              <NavLink to="/Product" onClick={() => setSelectedCategory("")}>
-                All Products
-              </NavLink>
-              <NavLink
-                to="/Homemade"
-                onClick={() => setSelectedCategory("Homemade")}
-              >
-                Homemade
-              </NavLink>
-              <NavLink
-                to="/Recycling"
-                onClick={() => setSelectedCategory("Recycling")}
-              >
-                Recycling
-              </NavLink>
-              <NavLink
-                to="/Hygienic"
-                onClick={() => setSelectedCategory("Hygienic")}
-              >
-                Hygienic
-              </NavLink>
-            </nav>
-          </div>
-          {/* <ul>
-  {getFilteredProducts().map((product) => (
-    <li key={product.id}>
-      {product.name} - {product.category.name}
-    </li>
-  ))}
-</ul> */}
-
-          <div className="Categories-dropdown">
-            <select
-              id="categorySelect"
-              value={selectedCategory}
-              onChange={handleCategoryChange}
-            >
-              <option value="">All Products</option>
-              <option value="Homemade">Homemade</option>
-              <option value="Recycling">Recycling</option>
-              <option value="Hygienic">Hygienic</option>
-            </select>
-          </div>
-
-          {/* <div className="search-container">
-          <input type="text" placeholder="Search..." onChange={handleSearchChange} />
-          <button>Search</button>
-        </div> */}
-          <div className="sort-options">
-            <label htmlFor="sort-select" className="sort">
-              Sort by:
-            </label>
-            <select id="sort-select" onChange={handleSortOptionChange}>
-              {/* <option value="price-asc" style={{display:"none"}}>Sort by:</option> */}
-              <option value="price-asc">Price: Low to High</option>
-              <option value="price-desc">Price: High to Low</option>
-              <option value="title-asc">Title: A to Z</option>
-              <option value="title-desc">Title: Z to A</option>
-            </select>
-          </div>
+    <div className="bags_products">
+  <div className="single_product">
+      <div className="product_img">
+        <img className="w_100" src={src} alt="" />
+      </div>
+      <div className="product_content">
+     
+        <h6>{name}</h6>
+        <div className="d-flex align-items-center justify-content-between">
+        <div className="rating_product text-center">
+          <span>
+            <i className="ri-star-s-fill"></i>
+          </span>
+          <span>
+            <i className="ri-star-s-fill"></i>
+          </span>
+          <span>
+            <i className="ri-star-s-fill"></i>
+          </span>
+          <span>
+            <i className="ri-star-s-fill"></i>
+          </span>
+          <span>
+            <i className="ri-star-s-fill"></i>
+          </span>
         </div>
-
-        <div className="product-container">
-          <div className="product-gallery">
-            {getSortedProducts().map((item) => (
-              <div className="item" key={item._id}>
-                <div className="thumbnail">
-                  <img
-                    className="group"
-                    src={`http://localhost:8000${item.dishImage}`}
-                    alt={item.name}
-                    width="100%"
-                    height="363"
-                  />
-                  <div className="caption">
-                    <h4 className="group-inner">{item.name}</h4>
-                    <p className="lead">${item.price}</p>
-                  </div>
-                  <div className="product-rate">
-                    {item.reviews && item.reviews.length > 0 ? (
-                      <div className="rating-and-reviews">
-                        <div className="rating">
-                          {[...Array(5)].map((_, index) => (
-                            <span
-                              className={`star ${
-                                index < Math.floor(item.reviews[0].rating)
-                                  ? "filled"
-                                  : ""
-                              }`}
-                              key={index}
-                            >
-                              <FontAwesomeIcon icon={faStar} />
-                            </span>
-                          ))}
-                        </div>
-                        <div className="rate-number">
-                          <p>{`${item.reviews.length} review(s)`}</p>
-                        </div>
-                      </div>
-                    ) : (
-                      <div className="no-review">No reviews </div>
-                    )}
-                  </div>
-
-                  {/* </div> */}
-                  <div className="btn-group">
-                    <NavLink
-                      to={{
-                        pathname: `/OneProduct/${item._id}`,
-                        // search: `?allproduct=${JSON.stringify(allproduct)}`,
-                      }}
-                      className="btn btn-details"
-                    >
-                      View
-                    </NavLink>
-
-                    <NavLink to="/" className="btn btn-success">
-                      Add to Cart
-                    </NavLink>
-                  </div>
-                  <NavLink to="/" className="btn btn-icon">
-                    <FontAwesomeIcon icon={faCartShopping} />
-                  </NavLink>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="pagination">
-          <a href="#">&laquo;</a>
-          <a href="#" class="active">
-            1
-          </a>
-          <a href="#">2</a>
-          <a href="#">3</a>
-          <a href="#">4</a>
-          <a href="#">5</a>
-          <a href="#">6</a>
-          <a href="#">&raquo;</a>
+          <span className="price_product">
+          <span>  <h3 style={{color:"beige"}}>price:</h3> {price}</span> <span style={{color:"red",
+         
+        }}className='shopping_icons'>
+            <i className="ri-shopping-cart-fill"></i>
+          </span>
+          </span>
+         
         </div>
       </div>
-    </div>
+    </div></div>
   );
-}
-export default Product;
+};
+
+export default newdishCard;
--- src/components/newdishes/newdishs.jsx
+++ src/components/newdishes/newdishs.jsx
@@ -1,83 +1,79 @@
-import React from "react";
-import "./newdish.css";
-import Sweet1 from "../../images/sweet1.png";
-import Sweet2 from "../../images/sweet2.png";
-import Sweet3 from "../../images/sweet3.png";
-import Sweet4 from "../../images/sweet4.png";
-import Sweet5 from "../../images/sweet5.png";
-import Sweet6 from "../../images/sweet6.png";
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import 'remixicon/fonts/remixicon.css';
+import './cart.css';
+import Sweet1 from '../../images/sweet1.png';
+import Sweet2 from '../../images/sweet2.png';
+import Sweet3 from '../../images/sweet3.png';
+import Sweet4 from '../../images/sweet4.png';
+import Sweet5 from '../../images/sweet5.png';
+import Sweet6 from '../../images/sweet6.png';
+import NewdishCard from './newdishCard.jsx';
 
 const arrayArrival = [
   {
     src: Sweet1,
-    alt: "Crème Brûlée",
-    name: "Crème Brûlée",
-    // category: "Category 1",
-    price: "$45.95",
+    alt: 'Crème Brûlée',
+    name: 'Crème Brûlée',
+    price: '$45.95',
   },
   {
     src: Sweet2,
-    alt: "Banoffee Pie",
-    name: "Banoffee Pie",
-    price: "$20.95",
+    alt: 'Banoffee Pie',
+    name: 'Banoffee Pie',
+    price: '$20.95',
   },
   {
     src: Sweet3,
-    alt: "cake",
-    name: "Chocolate Lava Cake",
-    price: "$12.50",
+    alt: 'Chocolate Lava Cake',
+    name: 'Chocolate Lava Cake',
+    price: '$12.50',
   },
   {
     src: Sweet4,
-    alt: "Chocolate Chip Cookies",
-    name: "Love Cookies",
-    // category: "Category 4",
-    price: "$12.50",
+    alt: 'Love Cookies',
+    name: 'Love Cookies',
+    price: '$12.50',
   },
   {
     src: Sweet5,
-    alt: "Strawberry Cheesecake",
-    name: "Strawberry Cheesecake",
-    // category: "Category 4",
-    price: "$12.50",
+    alt: 'Strawberry Cheesecake',
+    name: 'Strawberry Cheesecake',
+    price: '$12.50',
   },
   {
     src: Sweet6,
-    alt: "Donuts",
-    name: "Donuts",
-    price: "$12.50",
+    alt: 'Donuts',
+    name: 'Donuts',
+    price: '$12.50',
   },
- 
 ];
 
-function ArrivalhomePage() {
+export default function Newdishs() {
   return (
-    <main className="about_parent">
-      <hr className="about_hr" />
-      <div className="about_container">
-    
-        <section className="about_our_blog_container">
-          <div className="about_title_blog">
-            <h2 className="ourBloc_title">our Desserts</h2>
-          </div>
-          <div className="about_mission_container">
+    <section className="pt_0">
+    <Container>
+      <Row>
+        <Col className="text-center mb-4">
+          <h2>Popular Food menu</h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col lg="8">
+          <div className="cart-container">
             {arrayArrival.map((item, index) => (
-              <div className="mission_box" key={index}>
-                <img className="about_img" src={item.src} alt={item.alt} />
-                <span className="pay-value">{item.price}</span>
-                <div className="product-category">
-                  <span className="product-name">{item.name}</span>
-                  <span className="category-name">{item.category}</span>
-                </div>
+              <div key={index} className="single-product">
+                <NewdishCard item={item} />
               </div>
             ))}
           </div>
-          <h3 ><a className="new-dish-title" herf="#">menu ⥴
+        </Col>
+      </Row>
+    </Container>
+    <h3 ><a className="new-dish-title" herf="#">menu ⥴
 </a></h3>
-        </section>
-      </div>
-    </main>
+
+  </section>
+
   );
 }
-
-export default ArrivalhomePage;
